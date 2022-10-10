02e9b3cdd98b5e595c8463f5ae88d19abb6f3900
天天向上 <5673515+make_progress_every_day1@user.noreply.gitee.com>
2022-10-10 10:32:59 +0800

    block-style-collect init

---
+++ block-style-collect/js/content-script.js
@@ -0,0 +1,33 @@
+﻿/**
+ * content_scripts里更改标签页面的js，插件js不能操作打开标签页的dom
+ * */
+(function () {
+	class contentScript {
+		constructor() {
+			this.blockList = []; //所有的区块
+
+			this.init();
+		}
+		init() {
+			// 获取页面所有区块
+			this.getPageBlock($("body"));
+		}
+		getPageBlock(domContainer) {
+			domContainer.find("div[data-appid]").each((index, item) => {
+				let appid = $(item).attr("data-appid");
+				this.blockList.push(appid);
+			})
+		}
+	}
+
+	chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+		console.log('11111111');
+		if (request == "init-block") {
+			let _contentScript = new contentScript();
+			sendResponse({ status: true,blockList: _contentScript.blockList});
+		}else{
+			sendResponse({ status: false });
+		}
+		return true;
+	});
+})();
---
+++ block-style-collect/js/popup.js
@@ -0,0 +1,62 @@
+
+
+class collectBlock {
+    constructor(submitBtn, selectBtn) {
+        this.submitBtn = submitBtn;
+        this.selectBtn = selectBtn;
+
+        this.blockCollectInit();
+    }
+
+    blockCollectInit() {
+        this.sendMessageToContentScript('init-block',(response) => {
+            console.log(response);
+            if(response && response.status){
+                insertBlockChoose(response.blockList);
+            }
+        });
+    }
+
+    sendMessageToContentScript(message, callback) {
+        chrome.tabs.query({
+          active: true,
+          currentWindow: true
+        }, function (tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, message, function (response) {
+            if (callback) callback(response);
+          });
+        });
+      }
+
+    insertBlockChoose(blockArr) {
+        let $select = $("<select></select>");
+        console.log(this.selectBtn);
+        if (blockArr && blockArr.length <= 0) {
+            this.selectBtn.html("");
+            this.submitBtn.remove();
+            return;
+        }
+        blockArr.forEach((item, index) => {
+            var $option = "<option value=" + item + "区块>" + item + "区块</option>";
+            $select.append($option);
+        })
+        this.selectBtn.html($select);
+
+        this.bindStyleCollect();
+    }
+
+    // 绑定收集
+    bindStyleCollect() {
+        this.submitBtn.click(function () {
+            console.log('收集中……');
+        });
+    }
+}
+
+
+
+(function () {
+    let submitBtn = $("#collectbtn");
+    let selectBtn = $("#plugin-select");
+    new collectBlock(submitBtn, selectBtn);
+})();
